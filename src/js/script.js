9dbf5ee7e8878e4323a36c9f65f206e906810a65
eduardo <eduardo@marcotec.com.br>
2025-03-28 11:42:06 -0300

    declaração de variaveis

---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+// alert("Hello World")
+// declaração de variaveis
+
+var nome ="Fiap"
+console.log(nome)
+
+let idade =19;
+console.log(idade)
+
+const sobrenome = "Tolentino"
+console.log(sobrenome)
+
+//undefined
+
+let exemplo;
+console.log(exemplo)
+
+let exemplo1 =null;
+console.log(exemplo1)
