6573ecbd00c7a305c4bdae5de0e2b104dfb276fb
eduardo <eduardo@marcotec.com.br>
2025-03-28 11:49:12 -0300

    tipos de variaveis

--- src/js/script.js
+++ src/js/script.js
@@ -17,3 +17,20 @@ console.log(exemplo)
 
 let exemplo1 =null;
 console.log(exemplo1)
+
+//TIPOS DE VARIAVEIS
+let ex1 ="fiap"
+console.log(typeof ex1)
+
+let ex2 =25;
+console.log(typeof ex2)
+
+let ex3 =true;
+console.log(typeof ex3)
+
+let ex4 ={}
+console.log(typeof ex4)
+
+let ex5 =[]
+console.log(typeof ex5)
+
