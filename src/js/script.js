4e206ad3e52a80b999f7cf25d21eec91ba82571b
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-04 10:51:57 -0300

    adicionando conversões

--- src/js/script.js
+++ src/js/script.js
@@ -1,36 +1,57 @@
 // alert("Hello World")
 // declaração de variaveis
 
-var nome ="Fiap"
-console.log(nome)
+// var nome ="Fiap"
+// console.log(nome)
 
-let idade =19;
-console.log(idade)
+// let idade =19;
+// console.log(idade)
 
-const sobrenome = "Tolentino"
-console.log(sobrenome)
+// const sobrenome = "Tolentino"
+// console.log(sobrenome)
 
-//undefined
+// //undefined
 
-let exemplo;
-console.log(exemplo)
+// let exemplo;
+// console.log(exemplo)
 
-let exemplo1 =null;
-console.log(exemplo1)
+// let exemplo1 =null;
+// console.log(exemplo1)
 
-//TIPOS DE VARIAVEIS
-let ex1 ="fiap"
-console.log(typeof ex1)
+// //TIPOS DE VARIAVEIS
+// let ex1 ="fiap"
+// console.log(typeof ex1)
 
-let ex2 =25;
-console.log(typeof ex2)
+// let ex2 =25;
+// console.log(typeof ex2)
 
-let ex3 =true;
-console.log(typeof ex3)
+// let ex3 =true;
+// console.log(typeof ex3)
 
-let ex4 ={}
-console.log(typeof ex4)
+// let ex4 ={}
+// console.log(typeof ex4)
 
-let ex5 =[]
-console.log(typeof ex5)
+// let ex5 =[]
+// console.log(typeof ex5)
 
+// CONVERSÕES
+
+//FLOAT -> STRING
+
+let numFloat = 123.456;
+console.log(numFloat.toString())
+
+// STRING -> FLOAT
+
+let numString = "12.3434"
+console.log(parseFloat(numString))
+
+//INT -> STRING
+
+let numInt = 10
+console.log(numInt.toString())
+
+//STRING -> INT
+
+let numString2 = "10"
+console.log(parseInt(numString2))
